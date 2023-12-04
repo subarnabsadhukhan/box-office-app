d0fb45eaeabf423ebd84f5de2a56817dc5a7c608
Subarnab Sadhukhan <git@subarnabsadhukhan.com>
2023-12-04 08:47:52 +0530

    use useQuery on Home page to fetch search data

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,37 +1,42 @@
 import { useState } from "react";
+import { useQuery } from "@tanstack/react-query";
 import { searchMovie } from "../api/omdbapi";
-import SearchForm from "../componenets/SearchForm";
 import { searchActors } from "../api/tvmaze";
+import SearchForm from "../componenets/SearchForm";
 import MoviesGrid from "../componenets/movies/MoviesGrid";
 import ActorsGrid from "../componenets/actors/ActorsGrid";
 
 function Home() {
-  const [apiData, setApiData] = useState(null);
-  const [apiDataError, setApiDataError] = useState(null);
+  const [filter, setFilter] = useState(null);
 
-  const onSearch = async ({ searchStr, searchOption }) => {
-    try {
-      setApiDataError(null);
-      if (searchOption === "movies") {
-        const result = await searchMovie(searchStr);
+  const { data: apiData, error: apiDataError } = useQuery({
+    queryKey: ["search", filter],
+    queryFn: async () => {
+      if (filter.searchOption === "movies") {
+        const result = await searchMovie(filter.searchStr);
         console.log(result);
 
         if (result.Response === "False") throw new Error(result.Error);
-        setApiData(result.Search);
-      } else if (searchOption === "actors") {
-        const result = await searchActors(searchStr);
+        return result.Search;
+      } else if (filter.searchOption === "actors") {
+        const result = await searchActors(filter.searchStr);
 
         if (result.length === 0) throw new Error(`Actor Not Found`);
-        setApiData(result);
+        return result;
       }
-    } catch (e) {
-      setApiDataError(e);
-    }
+    },
+    networkMode: "always",
+    enabled: !!filter,
+  });
+
+  const onSearch = async ({ searchStr, searchOption }) => {
+    setFilter({ searchStr, searchOption });
   };
   const renderApiData = () => {
     if (apiDataError) {
       return <div>Error: {apiDataError.message}</div>;
     }
+
     if (apiData) {
       return apiData.map((search) => {
         if (search.imdbID) {
