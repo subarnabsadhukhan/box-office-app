0471338280dcbb8f254b7b0d829d036a81f9992b
Subarnab Sadhukhan <git@subarnabsadhukhan.com>
2023-12-06 17:30:20 +0530

    show Starred Movies on Starred Page

--- src/api/omdbapi.js
+++ src/api/omdbapi.js
@@ -12,3 +12,8 @@ const apiGet = async function (queryString) {
 export const searchMovie = (searchStr) => apiGet(`s=${searchStr}&page=1`);
 
 export const showMovieDetails = (imdbID) => apiGet(`i=${imdbID}&plot=full`);
+
+export const showMovieDetailsByIDs = async (imdbIDArray) => {
+  const result = imdbIDArray.map((imdbID) => apiGet(`i=${imdbID}`));
+  return Promise.all(result);
+};
--- src/pages/Starred.jsx
+++ src/pages/Starred.jsx
@@ -1,8 +1,48 @@
+import { useQuery } from "@tanstack/react-query";
 import { useStarredMovies } from "../lib/useStarredMovies";
+import { showMovieDetailsByIDs } from "../api/omdbapi";
+import MoviesGrid from "../componenets/movies/MoviesGrid";
 
 function Starred() {
-  const [starredMovies] = useStarredMovies();
-  return <div>Starred Page {starredMovies.length} </div>;
+  const [starredMoviesIDs, dispatch] = useStarredMovies();
+  const starMeClick = (imdbID) => {
+    const isStarred = starredMoviesIDs.includes(imdbID);
+
+    dispatch({ type: isStarred ? "UNSTAR" : "STAR", imdbID });
+  };
+  const { data: starredMovies, error: starredMoviesError } = useQuery({
+    queryKey: ["search", starredMoviesIDs],
+    queryFn: () =>
+      showMovieDetailsByIDs(starredMoviesIDs).then((result) => {
+        console.log(result);
+
+        return result;
+      }),
+
+    networkMode: "always",
+    enabled: !!starredMoviesIDs,
+    refetchOnWindowFocus: false,
+  });
+
+  if (starredMovies && starredMovies.length === 0)
+    return <div>No Movies were Starred</div>;
+  if (starredMovies && starredMovies.length > 0)
+    return (
+      <div>
+        {starredMovies.map((movies) => (
+          <MoviesGrid
+            key={movies.imdbID}
+            search={movies}
+            starMeClick={starMeClick}
+            isStarred={starredMoviesIDs.includes(movies.imdbID)}
+          />
+        ))}
+      </div>
+    );
+  if (starredMoviesError)
+    return <div>Error Occurred: {starredMoviesError.message}</div>;
+
+  return <div>Data is Loading...</div>;
 }
 
 export default Starred;
